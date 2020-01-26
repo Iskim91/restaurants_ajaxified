fc7804708b1637150f9bd3918a70d82d646b6e4a
Isac Kim <jk724814@gmail.com>
2020-01-26 11:59:46 +0000

    FEAT: fxed the avatar problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :avatar, AvatarUploader
-  has_one_attached :avatar
   has_many :cocktails, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
