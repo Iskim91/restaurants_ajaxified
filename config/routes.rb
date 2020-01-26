58e6f474cf6f5e83715e86ddb749aaca98a306e9
Isac Kim <jk724814@gmail.com>
2020-01-26 00:00:09 +0000

    FEAT: changed the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # root to: 'cocktails#index'
-  get: '/' to: 'cocktails#index'
+  root to: 'cocktails#index'
   devise_for :users
   resources :users, only: [:show]
   resources :doses, only: [:destroy]
