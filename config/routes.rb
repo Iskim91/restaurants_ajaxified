bbaf7d8d63140f568919cd4e7c41ad1dc62c53e9
Isac Kim <jk724814@gmail.com>
2020-01-27 15:58:47 +0000

    error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+  devise_for :users
+  resources :users, only: [:show]
+  resources :doses, only: [:destroy]
+  resources :cocktails do
+    resources :reviews, only: :create
+    resources :doses, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
